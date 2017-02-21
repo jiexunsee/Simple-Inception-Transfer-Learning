d2229541b3347c2cf15964793f61ba2e1a72b23f
See Jie Xun <jxsee94@yahoo.com.sg>
2017-02-21 14:43:25 +0800

    Update inception_v3.py

--- inception_v3.py
+++ inception_v3.py
@@ -514,7 +514,7 @@ def inception_v3(inputs,
         # 1 x 1 x 2048
         net = slim.dropout(net, keep_prob=dropout_keep_prob, scope='Dropout_1b')
         end_points['PreLogits'] = net
-	# above is probably 2048 that we want
+	# above is 2048 dimensional vector we want. we return the above instead!
         #logits = slim.conv2d(net, num_classes, [1, 1], activation_fn=None, normalizer_fn=None, scope='Conv2d_1c_1x1')
         #if spatial_squeeze:
         #  logits = tf.squeeze(logits, [1, 2], name='SpatialSqueeze')
